7841dd0a9e17f03bbaeff61bf296c4119735cd99
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-30 19:53:33 -0300

    Feat: Insert info API (version, title, description)

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from ninja import NinjaAPI
 from catalog.core.api import router as catalog_router
 
-api = NinjaAPI()
+api = NinjaAPI(
+    version='1.0',
+    csrf=True,
+    title='API Wholesale Footwear Catalog',
+    description='This project contains backend REST APIs needed to support a web application that allows '
+                'a footwear industry to display wholesale catalog.',
+    urls_namespace='public_api',
+)
 api.add_router('/catalog/', catalog_router)
 
 urlpatterns = [
