85a32fce527c561c1d2928f86dff2372f66e8c17
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-30 01:44:46 -0300

    Feat: Create admin of models

--- catalog/core/admin.py
+++ catalog/core/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Category, Collection, Size, Status, Footwear, Photo
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class FootwearAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'abc_curve', 'status' )
+    search_fields = ('code',)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('code_footwear', 'url', 'extension')
+    search_fields = ('code_footwear',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Size, SizeAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Footwear, FootwearAdmin)
+admin.site.register(Photo, PhotoAdmin)
--- catalog/core/models.py
+++ catalog/core/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=100, help_text="Category of footwear.", verbose_name="Category.")
+    title = models.CharField(max_length=100, help_text="Category of footwear.", verbose_name="Category")
 
     def __str__(self):
         return self.title
 
 
 class Collection(models.Model):
-    title = models.CharField(max_length=100, help_text="Collection.", verbose_name="Collection.")
+    title = models.CharField(max_length=100, help_text="Collection.", verbose_name="Collection")
 
     def __str__(self):
         return self.title
@@ -30,15 +30,15 @@ class Size(models.Model):
 
 
 class Footwear(models.Model):
-    code = models.CharField(max_length=100, help_text="Code of footwear.", verbose_name="Code.")
-    upper = models.CharField(max_length=100, help_text="Upper/Leather material of footwear.", verbose_name="Upper.")
-    name = models.CharField(max_length=100, help_text="Title of footwear", verbose_name="Title.")
-    outsole = models.CharField(max_length=100, help_text="Outsole material of footwear.", verbose_name="Outsole.")
-    lining = models.CharField(max_length=100, help_text="Lining material of footwear.", verbose_name="Lining.")
-    shoelaces = models.CharField(max_length=100, help_text="Shoelaces material of footwear.", verbose_name="Shoelaces.")
-    insole = models.CharField(max_length=100, help_text="Insole material of footwear.", verbose_name="Insole.")
-    abc_curve = models.CharField(max_length=1, help_text="ABC curve of footwear.", verbose_name="Category.")
-    cost_price = models.FloatField(null=True, blank=True)
+    code = models.CharField(max_length=100, help_text="Code of footwear.", verbose_name="Code")
+    upper = models.CharField(max_length=100, help_text="Upper/Leather material of footwear.", verbose_name="Upper")
+    name = models.CharField(max_length=100, help_text="Title of footwear", verbose_name="Title")
+    outsole = models.CharField(max_length=100, help_text="Outsole material of footwear.", verbose_name="Outsole")
+    lining = models.CharField(max_length=100, help_text="Lining material of footwear.", verbose_name="Lining")
+    shoelaces = models.CharField(max_length=100, help_text="Shoelaces material of footwear.", verbose_name="Shoelaces")
+    insole = models.CharField(max_length=100, help_text="Insole material of footwear.", verbose_name="Insole")
+    abc_curve = models.CharField(max_length=1, help_text="ABC curve of footwear.", verbose_name="Curve")
+    cost_price = models.FloatField(null=True, blank=True, verbose_name="USD $")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE, blank=True, null=True)
     size = models.ForeignKey(Size, on_delete=models.CASCADE, blank=True, null=True)
@@ -50,11 +50,11 @@ class Footwear(models.Model):
 
 class Photo(models.Model):
     code_footwear = models.ForeignKey(Footwear, on_delete=models.CASCADE, blank=False, null=False)
-    title = models.CharField(max_length=100, help_text="Title of photo.", verbose_name="Title.")
-    url = models.URLField(max_length=200, help_text="URL of photo.", verbose_name="URL.")
-    thumb = models.URLField(max_length=200, help_text="URL thumb of photo.", verbose_name="Thumb.")
-    mime = models.CharField(max_length=100, help_text="Mime type of photo.", verbose_name="MIME.")
-    extension = models.CharField(max_length=10, help_text="Extension type of photo.", verbose_name="Extension.")
+    title = models.CharField(max_length=100, help_text="Title of photo.", verbose_name="Title")
+    url = models.URLField(max_length=200, help_text="URL of photo.", verbose_name="URL")
+    thumb = models.URLField(max_length=200, help_text="URL thumb of photo.", verbose_name="Thumb")
+    mime = models.CharField(max_length=100, help_text="Mime type of photo.", verbose_name="MIME")
+    extension = models.CharField(max_length=10, help_text="Extension type of photo.", verbose_name="Extension")
 
     def __str__(self):
         return self.url
