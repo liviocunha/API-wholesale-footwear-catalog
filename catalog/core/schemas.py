ec5c3b76b2523455a138208152c075655fdb1148
Lívio Cunha <liviocunha91@hotmail.com>
2021-03-30 10:59:12 -0300

    Feat: Create schemas Client and Category

--- catalog/core/schemas.py
+++ catalog/core/schemas.py
@@ -1,1 +1,18 @@
 from ninja import Schema
+
+
+class ClientSchema(Schema):
+    id: int
+    client: str
+    key: str
+
+
+class CategoryIn(Schema):
+    client: int
+    title: str
+
+
+class CategoryOut(Schema):
+    id: int
+    client: ClientSchema
+    title: str
