b70bcc82e7e7176fb5b85cd2843f6f00f86b395d
Lívio Cunha <liviocunha91@hotmail.com>
2021-04-01 16:41:27 -0300

    Refactor: Convert string Values of Dictionary in .title()

--- catalog/core/api.py
+++ catalog/core/api.py
@@ -4,7 +4,7 @@ from ninja import Router
 from ninja.security import django_auth, APIKeyQuery
 from .models import Client, Category, Collection
 from .schemas import ClientIn, ClientOut, CategoryIn, CategoryOut, CollectionIn, CollectionOut
-from .utils import generate_api_key, str_upper, str_title
+from .utils import generate_api_key, modified_dict_values_title
 
 
 class ApiKey(APIKeyQuery):
@@ -46,26 +46,28 @@ def list_client(request):
 @router.post("/category", auth=api_key, tags=["category"])
 def create_category(request, payload: CategoryIn):
     data = payload.dict()
+    data = modified_dict_values_title(data)
     client = Client.objects.get(key=request.auth.key)
     try:
         categories = Category.objects.filter(client=client)
-        get_category = categories.get(title=str_upper(data['title']))
-        return {"detail": f"The {str_title(data['title'])} category already exists, please insert a new one."}
+        get_category = categories.get(title__icontains=data['title'])
+
+        return {"detail": f"The {data['title']} category already exists, please insert a new one."}
     except Category.DoesNotExist:
-        category = Category.objects.create(client=client, title=str_upper(data['title']))
-        return {"id": category.id, "title": category.title.title()}
+        category = Category.objects.create(client=client, title=data['title'])
+
+        return {"id": category.id, "title": category.title}
 
 
 @router.get("/category/{title}", response=CategoryOut, auth=api_key, tags=["category"])
 def get_category(request, title: str):
-    title = str_upper(title)
     try:
         client = Client.objects.get(key=request.auth.key)
         categories = Category.objects.filter(client=client)
-        category_one = categories.get(title=str_upper(title))
+        category_one = categories.get(title__icontains=title)
         return category_one
     except Category.DoesNotExist:
-        return {'id': None, 'client': None, 'title': None, 'detail': 'No categories for this client'}
+        return {'id': None, 'client': None, 'title': None, 'detail': 'No categories for this Client'}
 
 
 @router.get("/category", response=List[CategoryOut], auth=api_key, tags=["category"])
@@ -79,26 +81,28 @@ def list_category(request):
 @router.post("/collection", auth=api_key, tags=["collection"])
 def create_collection(request, payload: CollectionIn):
     data = payload.dict()
+    data = modified_dict_values_title(data)
     client = Client.objects.get(key=request.auth.key)
     try:
         collections = Collection.objects.filter(client=client)
-        get_collection = collections.get(title=str_upper(data['title']))
-        return {"detail": f"The {str_title(data['title'])} collection already exists, please insert a new one."}
+        get_collection = collections.get(title__icontains=data['title'])
+
+        return {"detail": f"The {data['title']} collection already exists, please insert a new one."}
     except Collection.DoesNotExist:
-        collection = Collection.objects.create(client=client, title=str_upper(data['title']))
-        return {"id": collection.id, "title": str_title(collection.title)}
+        collection = Collection.objects.create(client=client, title=data['title'])
+
+        return {"id": collection.id, "title": collection.title}
 
 
 @router.get("/collection/{title}", response=CollectionOut, auth=api_key, tags=["collection"])
 def get_collection(request, title: str):
-    title = str_upper(title)
     try:
         client = Client.objects.get(key=request.auth.key)
         collections = Collection.objects.filter(client=client)
-        collection_one = collections.get(title=str_upper(title))
+        collection_one = collections.get(title__icontains=title)
         return collection_one
     except Collection.DoesNotExist:
-        return {'id': None, 'client': None, 'title': None, 'detail': 'No collection for this client'}
+        return {'id': None, 'client': None, 'title': None, 'detail': 'No collection for this Client'}
 
 
 @router.get("/collection", response=List[CollectionOut], auth=api_key, tags=["collection"])
--- catalog/core/utils.py
+++ catalog/core/utils.py
@@ -1,16 +1,19 @@
 import random
+from typing import Dict
 
 
 def generate_api_key():
+    """ Create API Key for using Client """
     chars = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
     size = 50
     api_key = "".join(random.sample(chars, size))
     return api_key
 
 
-def str_upper(string):
-    return string.upper()
+def modified_dict_values_title(data_dict: Dict):
+    """ Convert string Values of Dictionary in .title() """
+    modified_dict = {}
+    for k, v in data_dict.items():
+        modified_dict[k] = str(data_dict[k]).title()
+    return modified_dict
 
-
-def str_title(string):
-    return string.title()
