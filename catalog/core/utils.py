b28be064e08d3f2c1f8d755b8565debd36fbacfc
Lívio Cunha <liviocunha91@hotmail.com>
2021-04-12 16:07:51 -0300

    Feat: Function upload_image on imgbb storage

--- catalog/core/utils.py
+++ catalog/core/utils.py
@@ -1,4 +1,7 @@
 import random
+import base64
+import requests
+import sys
 from typing import Dict
 
 
@@ -17,3 +20,35 @@ def modified_dict_values_title(data_dict: Dict):
         modified_dict[k] = str(data_dict[k]).title()
     return modified_dict
 
+
+# Replace with your API key
+api_key = 'e9ba762472ed0b259ca49bb9f9892a6a'
+
+
+def upload_image(photo, code):
+    try:
+        with open(photo, "rb") as file:
+            url = "https://api.imgbb.com/1/upload"
+            payload = {
+                "key": api_key,
+                "image": base64.b64encode(file.read()),
+                "name": code,
+            }
+            res = requests.post(url, payload)
+        if res.status_code == 200:
+            # print("Server Response: " + str(res.status_code))
+            # print("Image Successfully Uploaded")
+            return res
+        else:
+            print("ERROR")
+            print("Server Response: " + str(res.status_code))
+            return res
+    except FileNotFoundError as e:
+        print("FileNotFoundError:", e)
+        sys.exit()
+    except OSError as e:
+        print("OSError:", e)
+        sys.exit()
+    except Exception as e:
+        print(type(e), e)
+        sys.exit()
